354879c59c0b46efe1bba40e12c1000d5033e510
Mark Mestern <mark.mestern@uswitch.com>
2022-10-20 17:22:17 +0100

    daily commit

--- src/neville/core.clj
+++ src/neville/core.clj
@@ -30,7 +30,28 @@
       (= 1 power)  (format "%d*x %s %s" coefficient connector rest-poly)
       :else        coefficient)))
 
+(defn normalise-poly
+  "Remove 0 at end of poly"
+  [p]
+  (cond (<= (count p) 1) p
+        (not= 0 (last p)) p
+        :else (normalise-poly (drop-last p))))
+
+(defn add-poly
+  [p1 p2]
+  (let [max-length (max (count p1) (count p2))
+        p1*        (take max-length (concat p1 (repeat 0)))
+        p2*        (take max-length (concat p2 (repeat 0)))]
+    (normalise-poly (mapv + p1* p2*))))
+
+(defn multiply-poly
+  [p1 p2])
+
 (print-poly [1 2 -4])
+
+(normalise-poly [1 2 -4 0 0])
+
+(add-poly [1 2] [1 2 5 0])
 
 (defn -main
   "I don't do a whole lot ... yet."
